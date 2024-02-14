67584b6ac723e1c12c288a8c74597b3c8171b8fa
Joshua David <joshuadavidang@outlook.sg>
2024-02-14 08:11:52 +0800

    refactor: clean up routes (#2)

--- authentication_service/src/router/index.ts
+++ authentication_service/src/router/index.ts
@@ -8,14 +8,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const router = new Router();
-const API_VERSION = process.env.API_VERSION;
+const API_VERSION = String(process.env.API_VERSION);
+router.prefix(API_VERSION);
 
 router.get(`${API_VERSION}/`, async (ctx: any) => {
   ctx.body = "Server is Running! 💨";
 });
 
-router.get(`${API_VERSION}/auth`, authenticateUser);
-router.get(`${API_VERSION}/redirect`, redirectUser);
-router.get(`${API_VERSION}/userData`, getUserData);
+router.get("/auth", authenticateUser);
+router.get("/redirect", redirectUser);
+router.get("/userData", getUserData);
 
 export default router;
