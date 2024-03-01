64446cfa73496a3eac83b29fabb5edb7b62477b6
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-03-01 11:44:16 +0800

    payment feature, api hidden

---
+++ payment_service/payment.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, request, jsonify
+from os import environ
+
+import stripe
+
+app = Flask(__name__)
+app.config["STRIPE_SECRET_KEY"] = environ.get("api_key")
+
+stripe.api_key = app.config["STRIPE_SECRET_KEY"]
+
+app = Flask(__name__, static_url_path='', static_folder='./payment_test')
+
+YOUR_DOMAIN = 'http://localhost:5001'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1Onmq1FBfCxiguYfQ7QJVpcw',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            invoice_creation={"enabled": True},
+            success_url=YOUR_DOMAIN + '/success.html?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+
+        session_id = checkout_session.id
+        customer_email = checkout_session.get('customer_email')
+        session = stripe.checkout.Session.retrieve(session_id)
+        print("email", customer_email)
+        session['customer_email'] = customer_email
+
+        print('Generated Session ID:', session_id)
+        print("session", session)
+        
+    except Exception as e:
+        return str(e)
+    
+    return redirect(checkout_session.url, code=303)
+
+
+
+# call notif, event qty
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
