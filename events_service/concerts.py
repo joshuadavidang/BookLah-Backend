e6e24a55e0b876c13d62588ce11a44d483b30f4c
joshuadavidang <joshuadavidang@outlook.sg>
2024-03-02 18:56:14 +0800

    feat: code clean up
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- events_service/concerts.py
+++ events_service/concerts.py
@@ -1,35 +1,112 @@
-from flask import Flask
-from flask_cors import CORS
-from config import init_db
-from flask import jsonify
+from flask import request, jsonify
+from dbConfig import app, db, PORT
 
-app = Flask(__name__)
-PORT = 5001
-CORS(app, supports_credentials=True)
+
+class Events(db.Model):
+    __tablename__ = "concert"
+    concertid = db.Column(db.Integer, primary_key=True)
+    performer = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
+    venue = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.String(150), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+
+    def __init__(
+        self,
+        concertid,
+        performer,
+        title,
+        venue,
+        date,
+        time,
+        capacity,
+        description,
+    ):
+        self.concertid = concertid
+        self.performer = performer
+        self.title = title
+        self.venue = venue
+        self.date = date
+        self.time = time
+        self.capacity = capacity
+        self.description = description
+
+    def json(self):
+        return {
+            "concertid": self.concertid,
+            "performer": self.performer,
+            "title": self.title,
+            "venue": self.venue,
+            "date": self.date,
+            "time": self.time,
+            "capacity": self.capacity,
+            "description": self.description,
+        }
 
 
 @app.route("/getConcerts")
 def getConcerts():
-    conn = init_db.getDbConnection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM concerts;"
-    cursor.execute(query)
+    concerts = db.session.scalars(db.select(Events)).all()
 
-    concerts = cursor.fetchall()
+    if len(concerts):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {"concerts": [concert.json() for concert in concerts]},
+            }
+        )
+    return jsonify({"code": 404, "message": "There are no concerts."}), 404
 
-    if concerts:
-        columns = [column[0] for column in cursor.description]
-        concerts = [
-            {columns[i]: concert[i] for i in range(len(columns))}
-            for concert in concerts
-        ]
-        cursor.close()
-        conn.close()
-        return jsonify({"code": 200, "data": concerts}), 200
 
-    cursor.close()
-    conn.close()
-    return jsonify({"code": 404, "message": "Data not found"}), 404
+@app.route("/getConcert/<string:concertid>")
+def getConcert(concertid):
+
+    concert = db.session.scalars(
+        db.select(Events).filter_by(concertid=concertid).limit(1)
+    ).first()
+
+    if concert:
+        return jsonify({"code": 200, "data": concert.json()})
+    return jsonify({"code": 404, "message": "concert not found."}), 404
+
+
+@app.route("/addConcert/<string:concertid>", methods=["POST"])
+def addConcert(concertid):
+    if db.session.scalars(
+        db.select(Events).filter_by(concertid=concertid).limit(1)
+    ).first():
+        return (
+            jsonify(
+                {
+                    "code": 400,
+                    "data": {"concertid": concertid},
+                    "message": "Concert already exists.",
+                }
+            ),
+            400,
+        )
+
+    data = request.get_json()
+    concert = Events(concertid, **data)
+
+    try:
+        db.session.add(concert)
+        db.session.commit()
+    except:
+        return (
+            jsonify(
+                {
+                    "code": 500,
+                    "data": {"concertid": concertid},
+                    "message": "An error occurred creating the concert.",
+                }
+            ),
+            500,
+        )
+
+    return jsonify({"code": 201, "data": concert.json()}), 201
 
 
 if __name__ == "__main__":
