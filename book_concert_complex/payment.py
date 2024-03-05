585f678640ac18e6bbe424b0afc2d99d02323953
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-03-05 18:15:11 +0800

    added process_payment function, requirements.txt

--- book_concert_complex/payment.py
+++ book_concert_complex/payment.py
@@ -18,7 +18,15 @@ app = Flask(__name__, static_url_path='', static_folder='./payment_test')
 
 YOUR_DOMAIN = 'http://localhost:5002'
 
-@app.route('/create-checkout-session', methods=['POST'])
+@app.route('/process_payment', methods=['POST'])
+def process_payment():
+    # get json data from frontend 
+    # import events db to generate price id? -> create line items array dynamically 
+
+    session = create_checkout_session()
+
+    return jsonify({"checkout_url": session.url})
+
 def create_checkout_session():
     configure()    
 
@@ -36,19 +44,6 @@ def create_checkout_session():
             success_url=YOUR_DOMAIN + '/success.html?session_id={CHECKOUT_SESSION_ID}',
             cancel_url=YOUR_DOMAIN + '/cancel.html',
         )
-
-        # session_id = checkout_session.id
-        # customer_email = checkout_session.get('customer_email')
-        # session = stripe.checkout.Session.retrieve(session_id)
-        # print("email", customer_email)
-        # session['customer_email'] = customer_email
-
-        # print('Generated Session ID:', session_id)
-        # print("session", session)
-
-        # app.logger.info('Generated Session ID: %s', session_id)
-        # app.logger.info("Email: %s", customer_email)
-        # app.logger.info("Session: %s", session)
         
     except Exception as e:
         return str(e)
@@ -61,9 +56,6 @@ def session_status():
   session = stripe.checkout.Session.retrieve(request.args.get('session_id'))
 
   return jsonify(status=session.status, customer_email=session.customer_details.email)
-
-# call notif, event qty
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
