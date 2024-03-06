2e6f0989f71892adfd2612c18a8c619d28c96bdf
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-03-06 13:09:42 +0800

    remove comments, edited json param for invoke_http

--- book_concert_complex/book_concert.py
+++ book_concert_complex/book_concert.py
@@ -15,14 +15,14 @@ app = Flask(__name__)
 CORS(app)
 
 event_URL = "http://localhost:5000/getConcerts"
-booking_URL = "http://localhost:5001/order"
-payment_URL = "http://localhost:5002/create-checkout-session"
+booking_URL = "http://localhost:5001/create_booking" 
+payment_URL = "http://localhost:5002/process_payment"
 notification_URL = "http://localhost:5003/notification"
 tracking_URL = "http://localhost:5004/tracking"
 activity_log_URL = "http://localhost:5005/activity_log"
 error_URL = "http://localhost:5006/error"
 
-exchangename = environ.get('exchangename') #order_topic
+exchangename = environ.get('exchangename')
 exchangetype = environ.get('exchangetype')
 
 connection = amqp_connection.create_connection() 
@@ -31,23 +31,20 @@ channel = connection.channel()
 #if the exchange is not yet created, exit the program
 if not amqp_connection.check_exchange(channel, exchangename, exchangetype):
     print("\nCreate the 'Exchange' before running this microservice. \nExiting the program.")
-    sys.exit(0)  # Exit with a success status
+    sys.exit(0)  
 
 @app.route("/book_concert", methods=['POST'])
 def book_concert():
-    # Simple check of input format and data of the request are JSON
     if request.is_json:
         try:
+            #booking (from frontend)
             booking = request.get_json()
             print("\nReceived an order in JSON:", booking)
 
-            # do the actual work
-            # 1. Send order info {cart items}
             result = processBookConcert(booking)
             return jsonify(result), result["code"]
 
         except Exception as e:
-            # Unexpected error in code
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
@@ -58,7 +55,6 @@ def book_concert():
                 "message": "book_concert.py internal error: " + ex_str
             }), 500
 
-    # if reached here, not a JSON request.
     return jsonify({
         "code": 400,
         "message": "Invalid JSON input: " + str(request.get_data())
@@ -66,28 +62,21 @@ def book_concert():
 
 
 def processBookConcert(booking):
-    # 2. Send the order info {cart items}
-    # Invoke the booking microservice
     print('\n-----Invoking booking microservice-----')
-    booking_result = invoke_http(booking_URL, method='POST', json=booking)
+    # takes json from frontend and creates a booking 
+    booking_result = invoke_http(booking_URL, method='POST', json=booking["data"])
     print("booking result :", booking_result)
 
-    # Check the order result; if a failure, send it to the error microservice.
     code = booking_result["code"]
+    message = json.dumps(booking_result)
     if code not in range(200, 300):
-        # Inform the error microservice
         print('\n\n-----Publishing the (booking error) message with routing_key=booking.error-----')
-        message = json.dumps(booking_result)
         channel.basic_publish(exchange=exchangename, routing_key="booking.error", 
             body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
         # make message persistent within the matching queues until it is received by some receiver 
-        # (the matching queues have to exist and be durable and bound to the exchange)
 
-        # - reply from the invocation is not used; 
-        # continue even if this invocation fails
         print("Booking status ({:d}) published to the RabbitMQ Exchange:".format(code), booking_result)
 
-        # 7. Return error
         return {
             "code": 500,
             "data": {"booking_result": booking_result},
@@ -97,7 +86,6 @@ def processBookConcert(booking):
     else:
         print('\n\n-----Publishing the (booking info) message with routing_key=booking.info-----')        
 
-        # invoke_http(activity_log_URL, method="POST", json=order_result)            
         channel.basic_publish(exchange=exchangename, routing_key="booking.info", 
             body=message)
     
@@ -105,7 +93,7 @@ def processBookConcert(booking):
 
 
     print('\n\n-----Invoking payment microservice-----')
-    payment_result = invoke_http(payment_URL, method="POST", json=booking_result['data'])
+    payment_result = invoke_http(payment_URL, method="POST", json=booking["data"])
     print("payment_result:", payment_result, '\n')
 
     code = payment_result["code"]
@@ -119,14 +107,15 @@ def processBookConcert(booking):
 
         return {
             "code": 500,
-            "data": {"booking_result": booking_result,
+            "data": {
                      "payment_result": payment_result},
             "message": "Simulated payment error sent for error handling."
         }
     
     
     print('\n\n-----Invoking notification microservice-----')
-    notification_result = invoke_http(notification_URL, method="POST", json=booking_result['data'])
+    #send email from payment and booking details from booking
+    notification_result = invoke_http(notification_URL, method="POST", json=jsonify({"email": payment_result["email"], "booking_details": booking_result["data"]}))
     print("notification_result:", notification_result, '\n')
 
     code = notification_result["code"]
