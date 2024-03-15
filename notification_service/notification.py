0dc3e7dc12f15b741002cd745cc7b10f4bbe67aa
jcjc13 <107243570+jcjc13@users.noreply.github.com>
2024-03-15 16:05:07 +0800

    Notification branch (#33)
    * created notification microservice
    
    * created notification microservice
    
    * Update requirements.txt
    
    * mircoservice complete
    
    * changed api
    
    * Update notification.py
    
    ---------
    
    Co-authored-by: joshuadavidang <joshuadavidang@outlook.sg>

--- notification_service/notification.py
+++ notification_service/notification.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sslify import SSLify
-import requests
 from flask_cors import CORS
+import mailtrap as mt
 from os import environ
 
 app = Flask(__name__)
+PORT = 5020
 CORS(app)
 sslify = SSLify(app)
-
-MAILGUN_API_KEY = environ.get("MAILGUN_API_KEY")
-MAILGUN_DOMAIN = environ.get("MAILGUN_DOMAIN")
+API_KEY = environ.get("API_KEY")
 
 
 @app.route("/api/v1/send-email", methods=["POST"])
@@ -17,23 +16,23 @@ def send_email():
     if request.method == "POST":
         data = request.get_json()
         recipient_email = data.get("recipient_email")
-        subject = data.get("subject")
         message = data.get("message")
 
-        if not (recipient_email and subject and message):
+        if not (recipient_email and message):
             return jsonify({"error": "Missing required parameters"}), 400
 
         try:
-            response = requests.post(
-                f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-                auth=("api", MAILGUN_API_KEY),
-                data={
-                    "from": "joshuadavidang@outlook.sg",
-                    "to": "joshuadavidang@outlook.sg",
-                    "subject": "subject",
-                    "text": "message",
-                },
+            mail = mt.Mail(
+                sender=mt.Address(email="mailtrap@demomailtrap.com", name="BookLah"),
+                to=[mt.Address(email=recipient_email)],
+                subject="Order Confirmation",
+                text=message,
+                category="Integration Test",
             )
+
+            client = mt.MailtrapClient(token=API_KEY)
+            client.send(mail)
+
             return jsonify({"message": "Email sent successfully"}), 200
 
         except Exception as e:
@@ -41,4 +40,4 @@ def send_email():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5020, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True)
