31488e5f026aec5e37881f77539240f0e5004f82
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-03-16 17:35:18 +0800

    create stripe ids and store in db

--- payment_service/payment.py
+++ payment_service/payment.py
@@ -33,6 +33,11 @@ class StripeIds(db.Model):
     price_id = db.Column(db.String(100), nullable=False)
     product_id = db.Column(db.String(100), nullable=False)
 
+    # __table_args__ = (
+    #         {"extend_existing": True},  # Extend existing table args
+    #         {"primary_key": (concert_id, category)}  # Define composite primary key
+    #     )
+    
     def json(self):
         return {
             "concert_id": self.concert_id,
@@ -43,13 +48,13 @@ class StripeIds(db.Model):
         }
 
 
-@app.route("/payment/<string:concert_id>/<string:category>")
+@app.route("/payment/get_stripeids/<uuid:concert_id>/<string:category>")
 def get_stripeids(concert_id, category):
-    stripe_ids = (
-        db.session.query(StripeIds)
-        .filter_by(concert_id=concert_id, category=category)
-        .first()
-    )
+    stripe_ids = db.session.scalars(
+            db.select(StripeIds).filter_by(concert_id=concert_id, category=category).
+            limit(1)
+    ).first()
+
     if stripe_ids:
         return jsonify({"code": 200, "data": stripe_ids.json()})
     return (
@@ -63,7 +68,6 @@ def get_stripeids(concert_id, category):
         404,
     )
 
-
 @app.route("/api/v1/getCustomerEmail", methods=["POST"])
 def getCustomerInfo():
     session_id = request.get_json().get("sessionId")
@@ -71,54 +75,54 @@ def getCustomerInfo():
     email = checkout_session.customer_details.email
     return jsonify({"code": 200, "email": email})
 
-
 @app.route("/api/v1/processPayment", methods=["POST"])
 def create_session():
 
-    # concert_id = request.json.get("concert_id", None)
-    # category = request.json.get("category", None)
-    # quantity = request.json.get("quantity", None)
+    concert_id = request.json.get("concert_id", None)
+    category = request.json.get("category", None)
+    quantity = request.json.get("quantity", None)
 
-    # price = result["price_id"]
-    concert_name = "test"
-    result = create_stripeids(concert_name)
+    stripeids = get_stripeids(concert_id, category)["data"]
 
-    return jsonify(result)
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    "price": stripeids["price_id"],
+                    "quantity": quantity,
+                },
+            ],
+            mode="payment",
+            invoice_creation={"enabled": True},
+            success_url=FRONT_END_DOMAIN + "/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=FRONT_END_DOMAIN + "/error",
+        )
 
-    # try:
-    #     checkout_session = stripe.checkout.Session.create(
-    #         line_items=[
-    #             {
-    #                 "price": "price_1OsSEpFBfCxiguYffsBlmZ5m",
-    #                 "quantity": 1,
-    #             },
-    #         ],
-    #         mode="payment",
-    #         invoice_creation={"enabled": True},
-    #         success_url=FRONT_END_DOMAIN + "/success?session_id={CHECKOUT_SESSION_ID}",
-    #         cancel_url=FRONT_END_DOMAIN + "/error",
-    #     )
+    except Exception as e:
+        return str(e)
 
-    # except Exception as e:
-    #     return str(e)
-
-    # return jsonify({"checkout_url": checkout_session.url})
+    return jsonify({"checkout_url": checkout_session.url})
 
 
-
-
+## ADMIN
 #create stripe ids from Stripe
-def create_stripeids(product_name):
+def create_stripeids(product_name, price):
 
     price = stripe.Price.create(
         currency="sgd",
-        unit_amount=1000, #in cents
+        unit_amount=price * 100, #in cents
         product_data={"name": product_name},
         )
 
-    return {"price_id": price["id"], "product_id": price["product"]}
-
+    return  {
+                "code": 201,
+                "data": {"price_id": price["id"],
+                            "product_id": price["product"]},
+                "message": "Stripe IDs created successfully",
+            }
+   
 #add Stripe IDs to database
+@app.route("/api/v1/add_stripeids/<uuid:concert_id>/<string:category>", methods=["POST"])
 def add_stripeids(concert_id, category):
     if db.session.scalars(
         db.select(StripeIds).filter_by(concert_id=concert_id, category=category).limit(1)
@@ -135,11 +139,25 @@ def add_stripeids(concert_id, category):
             400,
         )
 
-    data = request.get_json()
-    stripe_ids = StripeIds(concert_id, category, **data)
+    # data = request.get_json()
+    # concert_name = data["name"]
+    # price = data["price"]
 
+    concert_name = "PLEASE"
+    price = 1000
+
+    stripeids = create_stripeids(concert_name, price)
+
+    stripeids_db = StripeIds(
+                                concert_id=concert_id,
+                                category=category,
+                                concert_name=concert_name,
+                                price_id=stripeids["data"]["price_id"],
+                                product_id=stripeids["data"]["product_id"]
+                            )
+    
     try:
-        db.session.add(stripe_ids)
+        db.session.add(stripeids_db)
         db.session.commit()
     except:
         return (
@@ -148,7 +166,7 @@ def add_stripeids(concert_id, category):
                     "code": 500,
                     "data": {"concert_id": concert_id,
                              "category": category},
-                    "message": "An error occurred creating the Stripe IDs.",
+                    "message": "An error occurred adding the Stripe IDs.",
                 }
             ),
             500,
@@ -158,7 +176,7 @@ def add_stripeids(concert_id, category):
         jsonify(
             {
                 "code": 201,
-                "data": stripe_ids.json(),
+                "data": stripeids_db.json(),
                 "message": "Stripe IDs have been added successfully",
             }
         ),
@@ -166,4 +184,4 @@ def add_stripeids(concert_id, category):
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5006, debug=True)
