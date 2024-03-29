d41d548e254f2bbafb4b13b2ed21f3abc6310693
faithkoh <142731228+faithkoh@users.noreply.github.com>
2024-03-29 14:54:14 +0800

    Feat/tracking service (#43)
    * added number_seats_tracker.py file
    
    * moved seatChoose.py and seatTracking.py
    
    * added function to update number of taken seats in seatTracking.py
    
    * change app route according to new primary keys
    
    * updated create tracking function in seatTracking.py
    
    * updated routes in seatChoosing.py according to primary keys
    
    * added update seat info function to seatChoosing.py

--- concert_service/seatTracking.py
+++ concert_service/seatTracking.py
@@ -10,7 +10,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-class Concert(db.Model):
+class ConcertTracking(db.Model):
     __tablename__ = "tracking"
     concertID = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.Integer, primary_key=True)
@@ -34,7 +34,7 @@ class Concert(db.Model):
 
 @app.route("/api/v1/tracking")
 def get_all():
-    tracking_list = db.session.execute("SELECT * FROM tracking").fetchall()
+    tracking_list = db.session.scalars(db.select(ConcertTracking)).all()
 
     if len(tracking_list):
         return jsonify(
@@ -43,62 +43,85 @@ def get_all():
                 "data": {"tracking": [tracking.json() for tracking in tracking_list]},
             }
         )
-    return jsonify({"code": 404, "message": "There are no concert records."}), 404
+    return jsonify({"code": 404, "message": "There are no concert tracking records."}), 404
 
 
-@app.route("/api/v1/tracking/<string:concertID>")
+@app.route("/api/v1/tracking/<string:concertID>/<string:category>")
 def find_by_concertID(concertID, category):
-    concert = db.session.execute(
-        "SELECT * FROM tracking WHERE concertID = :concertID AND category = :category LIMIT 1",
-        {"concertID": concertID, "category": category},
-    ).fetchone()
+    concertTracking = db.session.scalars(
+        db.select(ConcertTracking).filter_by(concertID = concertID, category = category).limit(1)).first()
 
-    if concert:
-        return jsonify({"code": 200, "data": concert.json()})
-    return jsonify({"code": 404, "message": "Concert not found."}), 404
+    if concertTracking:
+        return jsonify({"code": 200, "data": concertTracking.json()})
+    return jsonify({"code": 404, "message": "Concert tracking not found."}), 404
 
 
-@app.route("/tracking/<string:concertID>", methods=["POST"])
+@app.route("/tracking/<string:concertID>/<string:category>", methods=["POST"])
 def create_tracking(concertID, category):
 
-    existing_record = db.session.execute(
-        "SELECT * FROM tracking WHERE concertID = :concertID AND category = :category LIMIT 1",
-        {"concertID": concertID, "category": category},
-    ).fetchone()
-
-    if existing_record:
-        return (
-            jsonify(
-                {
-                    "code": 400,
-                    "data": {"concertID": concertID},
-                    "message": "Concert tracking record already exists.",
-                }
-            ),
-            400,
-        )
-
-    data = request.get_json()
-    concert_data = {"concertID": concertID, "category": data.get("category")}
-    concert = concert(**concert_data, **data)
+    concertID = request.json.get('concertID', None)
+    category = request.json.get('category', None)
+    tracking = ConcertTracking(concertID = concertID, category= category, takenSeats = 0)
 
     try:
-        db.session.add(concert)
+        db.session.add(tracking)
         db.session.commit()
-    except:
-        return (
-            jsonify(
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred while creating the tracking record. " + str(e)
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": tracking.json()
+        }
+    ), 201
+
+@app.route("/tracking/<string:concertID>/<string:category>", methods=['PUT'])
+def update_availseats(concertID, category):
+    try:
+        tracking = db.session.scalars(
+        db.select(ConcertTracking).filter_by(concertID=concertID, category= category).
+        limit(1)).first()
+        if not tracking:
+            return jsonify(
                 {
-                    "code": 500,
-                    "data": {"concertID": concertID, "category": category},
-                    "message": "An error occurred creating the concert.",
+                    "code": 404,
+                    "data": {
+                        "concertID": concertID, 
+                        "category": category
+                    },
+                    "message": "Concert not found."
                 }
-            ),
-            500,
-        )
+            ), 404
 
-    return jsonify({"code": 201, "data": concert.json()}), 201
-
+        data = request.get_json()
+        if 'takenSeats' in data:
+            current_taken_seats = ConcertTracking.query.first().takenSeats
+            new_taken_seats = current_taken_seats + 1
+            ConcertTracking.takenSeats = new_taken_seats
+            db.session.commit()
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {"takenSeats": new_taken_seats}
+                }
+            ), 200
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "concertID": concertID, 
+                    "category": category
+                },
+                "message": "An error occurred while updating the number of seats taken. " + str(e)
+            }
+        ), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
