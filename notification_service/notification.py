d25e0f395846e21776db158dd4e28bc995f59f51
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-04-02 22:00:23 +0800

    notif: added send refund emails endpoint

--- notification_service/notification.py
+++ notification_service/notification.py
@@ -7,9 +7,8 @@ from os import environ
 app = Flask(__name__)
 PORT = 5003
 CORS(app)
-sslify = SSLify(app)
+# sslify = SSLify(app)
 MAILTRAP_API_KEY = environ.get("MAILTRAP_API_KEY")
-
 
 @app.route("/api/v1/send_email", methods=["POST"])
 def send_email():
@@ -20,7 +19,7 @@ def send_email():
         subject = data.get("subject")
 
         if not (recipient_email and message and subject):
-            return jsonify({"error": "Missing required parameters"}), 400
+            return jsonify({"code": 400, "error": "Missing required parameters"})
 
         try:
             mail = mt.Mail(
@@ -34,7 +33,37 @@ def send_email():
             client = mt.MailtrapClient(token=MAILTRAP_API_KEY)
             client.send(mail)
 
-            return jsonify({"code": 200, "message": "Email sent successfully"}), 200
+            return jsonify({"code": 200, "message": "Email sent successfully"})
+
+        except Exception as e:
+            return jsonify({"code": 500, "message": str(e)})
+
+@app.route("/api/v1/send_refund_emails", methods=["POST"])
+def send_refund_emails():
+    if request.method == "POST":
+        data = request.get_json()
+
+        recipient_emails = data.get("recipient_email") #array of emails
+        message = data.get("message")
+        subject = data.get("subject")
+
+        if not (recipient_emails and message and subject):
+            return jsonify({"code": 400, "error": "Missing required parameters"})
+
+        try:
+            for email in recipient_emails:
+                mail = mt.Mail(
+                    sender=mt.Address(email="mailtrap@demomailtrap.com", name="BookLah"),
+                    to=[mt.Address(email=email)],
+                    subject=subject,
+                    text=message,
+                    category="Integration Test",
+                )
+
+                client = mt.MailtrapClient(token=MAILTRAP_API_KEY)
+                client.send(mail)
+
+            return jsonify({"code": 200, "message": "Refund emails sent successfully"})
 
         except Exception as e:
             return jsonify({"code": 500, "message": str(e)}), 500
