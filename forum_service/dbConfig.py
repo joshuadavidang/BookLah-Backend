067b657da5c904dff10ab7f8d0025212b195b1e0
joshuadavidang <joshuadavidang@outlook.sg>
2024-04-04 23:23:47 +0800

    fix: code clean up
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- forum_service/dbConfig.py
+++ forum_service/dbConfig.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_socketio import SocketIO
+
 import os
 
 app = Flask(__name__)
 PORT = 5007
 CORS(app, supports_credentials=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
